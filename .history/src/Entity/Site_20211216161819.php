7f610db123ae7d8c386a8019dc52974879707686
cedric-pardon <cedricp02@gmail.com>
2021-12-16 16:26:24 +0100

    US04 fin

---
+++ .history/src/Entity/Site_20211216161819.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SiteRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=SiteRepository::class)
+ */
+class Site
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private string $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private string $description;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $city;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $beforeImage;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $afterImage;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getBeforeImage(): ?string
+    {
+        return $this->beforeImage;
+    }
+
+    public function setBeforeImage(string $beforeImage): self
+    {
+        $this->beforeImage = $beforeImage;
+
+        return $this;
+    }
+
+    public function getAfterImage(): ?string
+    {
+        return $this->afterImage;
+    }
+
+    public function setAfterImage(string $afterImage): self
+    {
+        $this->afterImage = $afterImage;
+
+        return $this;
+    }
+}
