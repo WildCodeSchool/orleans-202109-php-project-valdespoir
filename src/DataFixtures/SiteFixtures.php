8455abe2376235b0d0540e4e68609f9adf13ee4c
Djoa <siongdjoa3@gmail.com>
2021-12-17 15:49:58 +0100

    add faker

--- src/DataFixtures/SiteFixtures.php
+++ src/DataFixtures/SiteFixtures.php
@@ -4,15 +4,20 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use Faker;
+use Faker\Factory;
+use App\Entity\Site;
 
 class SiteFixtures extends Fixture
 {
+    public const SITE_NUMBER = 10;
     public function load(ObjectManager $manager): void
     {
-            //$manager->persist($site);
-
-            //$manager->flush();
+        $faker = Factory::create("fr_FR");
+        for ($i = 0; $i < self::SITE_NUMBER; $i++) {
+            $site = new Site();
+            $site->setTitle($faker->city);
+            $manager->persist($site);
         }
+        $manager->flush();
     }
-
+}
