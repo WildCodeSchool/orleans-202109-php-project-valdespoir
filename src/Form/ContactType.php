dc853154d9486ec44355f5d3470ad0a240341afe
Djoa <siongdjoa3@gmail.com>
2021-12-22 11:37:51 +0100

    create form

---
+++ src/Form/ContactType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use App\Entity\Contact;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('field_name')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Contact::class
+            // Configure your form options here
+        ]);
+    }
+}
