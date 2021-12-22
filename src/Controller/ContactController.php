cd2a5f27e231bcfe7f3b7a2b0570efe67e9c8357
Djoa <siongdjoa3@gmail.com>
2021-12-22 14:09:13 +0100

    add in ContactController

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Contact;
+use App\Form\ContactType;
 
 class ContactController extends AbstractController
 {
@@ -13,8 +15,11 @@ class ContactController extends AbstractController
      */
     public function index(): Response
     {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+
         return $this->render('contact/index.html.twig', [
-            '' => '',
+            'form' => $form->createView()
         ]);
     }
 }
