0c3120751b844f2486ed1a6e3397d8087c76e86e
Djoa <siongdjoa3@gmail.com>
2021-12-22 12:07:05 +0100

    add ContactType

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -5,6 +5,9 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use App\Entity\Contact;
 
 class ContactType extends AbstractType
@@ -12,7 +15,11 @@ class ContactType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('field_name')
+        ->add('firstName', TextType::class, ['label' => 'Nom'])
+        ->add('lastName', TextType::class, ['label' => 'Prénom'])
+        ->add('subjet', TextType::class, ['label' => 'Sujet'])
+        ->add('email', EmailType::class, ['label' => 'E-mail'])
+        ->add('message', TextareaType::class, ['label' => 'Message']);
         ;
     }
 
@@ -20,7 +27,6 @@ class ContactType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Contact::class
-            // Configure your form options here
         ]);
     }
 }
