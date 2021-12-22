123eae6fab06cf0f69bba05b4d3c913c5b78323d
Djoa <siongdjoa3@gmail.com>
2021-12-22 10:23:05 +0100

    delete @import carousel

--- migrations/Version20211216093813.php
+++ migrations/Version20211216093813.php
@@ -20,12 +20,12 @@ final class Version20211216093813 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE sites (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, city VARCHAR(255) NOT NULL, date DATE NOT NULL, before_image VARCHAR(255) NOT NULL, after_image VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE site (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, city VARCHAR(255) NOT NULL, date DATE NOT NULL, before_image VARCHAR(255) NOT NULL, after_image VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE sites');
+        $this->addSql('DROP TABLE site');
     }
 }
