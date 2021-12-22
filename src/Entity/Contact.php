55b7246fd8c526be20a75eeb8397ed774cdf07d6
Djoa <siongdjoa3@gmail.com>
2021-12-22 10:56:59 +0100

    creation entity contact

--- src/Entity/Contact.php
+++ src/Entity/Contact.php
@@ -2,24 +2,19 @@
 
 namespace App\Entity;
 
-use App\Repository\ContactRepository;
-use Doctrine\ORM\Mapping as ORM;
-
+/**
+ */
 class Contact
 {
     /**
-     * @Assert\NotBlank
-     */
-    private string $firstname;
-
-    /**
-     * @Assert\NotBlank
      */
     private string $lastname;
 
     /**
-     * @Assert\NotBlank
-     * @Assert\Email()
+     */
+    private string $firstname;
+
+    /**
      */
     private string $email;
 
@@ -28,21 +23,8 @@ class Contact
     private string $subject;
 
     /**
-     * @Assert\NotBlank
      */
     private string $message;
-
-    public function getFirstname(): ?string
-    {
-        return $this->firstname;
-    }
-
-    public function setFirstname(string $firstname): self
-    {
-        $this->firstname = $firstname;
-
-        return $this;
-    }
 
     public function getLastname(): ?string
     {
@@ -52,6 +34,18 @@ class Contact
     public function setLastname(string $lastname): self
     {
         $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
 
         return $this;
     }
