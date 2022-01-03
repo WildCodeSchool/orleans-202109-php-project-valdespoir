c293b9d14722629c5b8554fa4bf9ae18bd0de979
Martin Lerude <martin.lerude@gmail.com>
2022-01-03 22:20:04 +0100

    final banner 1

--- src/Controller/PartnerController.php
+++ src/Controller/PartnerController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Repository\SiteRepository;
 
 class PartnerController extends AbstractController
 {
@@ -14,6 +13,6 @@ class PartnerController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', ['sites']);
+        return $this->render('partner/index.html.twig');
     }
 }
