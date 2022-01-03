7b955ab02fcc08d62b548cdf5a50736a3e1826e5
Martin Lerude <martin.lerude@gmail.com>
2022-01-03 21:53:22 +0100

    add banner

---
+++ src/Controller/PartnerController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PartnerController extends AbstractController
+{
+    /**
+     * @Route("/partner", name="home")
+     */
+    public function index(): Response
+    {
+        return $this->render('partner/index.html.twig');
+    }
+}
