5d67962f7d8e19d2779d5892abbaddfb3299c68c
Arthur GOUAUX <arthur.gouaux@gmail.com>
2022-01-04 21:06:24 +0100

    description depuis la bdd

--- src/DataFixtures/JobOfferFixtures.php
+++ src/DataFixtures/JobOfferFixtures.php
@@ -3,57 +3,39 @@
 namespace App\DataFixtures;
 
 use App\Entity\JobOffer;
+use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use Doctrine\Common\DataFixtures\DependentFixtureInterface;
-
-class JobOfferFixtures extends Fixture implements DependentFixtureInterface
+class JobOfferFixtures extends Fixture
 {
     public const JOBOFFER = [
         [
             'title' => 'Offre 1',
-            'description' => 'Emplacement géographique : Sandillon<br>
-            Tâches à effectuer : tailler des arbres et tondre la pelouse.<br>
-            Horaires : 7h-16h<br>
-            Offre disponible : dès aujourd\'hui.<br>
-            Contact : 00 00 00 00 00 ou test@valespoir.fr<br>");'
+            'description' => 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.'
         ],
 
         [
             'title' => 'Offre 2',
-            'description' => 'Emplacement géographique : Sandillon<br>
-            Tâches à effectuer : tailler des arbres et tondre la pelouse.<br>
-            Horaires : 7h-16h<br>
-            Offre disponible : dès aujourd\'hui.<br>
-            Contact : 00 00 00 00 00 ou test@valespoir.fr<br>");'
+            'description' => 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.'
         ],
 
         [
             'title' => 'Offre 3',
-            'description' => 'Emplacement géographique : Sandillon<br>
-            Tâches à effectuer : tailler des arbres et tondre la pelouse.<br>
-            Horaires : 7h-16h<br>
-            Offre disponible : dès aujourd\'hui.<br>
-            Contact : 00 00 00 00 00 ou test@valespoir.fr<br>");'
+            'description' => 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.'
         ],
     ];
 
     public function load(ObjectManager $manager): void
     {
-        foreach (self::JOBOFFER as $key => $jobOfferTitle) {
+        foreach (self::JOBOFFER as $key => $show) {
             $jobOffer = new JobOffer();
-            $jobOffer->setTitle($jobOfferTitle['title']);
-            $jobOffer->setDescription($jobOfferTitle['description']);
-            $manager->persist($jobOfferTitle);
-            $this->addReference('job_' . $key, $jobOfferTitle);
+
+            $jobOffer->setTitle($show['title']);
+            $jobOffer->setDescription($show['description']);
+            $jobOffer->setDate(new DateTime());
+            $manager->persist($jobOffer);
         }
         $manager->flush();
     }
 
-    public function getDependencies()
-    {
-        return [
-            ProgramFixtures::class
-        ];
-    }
 }
--- src/Entity/JobOffer.php
+++ src/Entity/JobOffer.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\JobOfferRepository;
+use DateTime;
+use DateTimeInterface;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
@@ -29,6 +31,11 @@ class JobOffer
      */
     private string $description;
 
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private DateTimeInterface $date;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -54,6 +61,18 @@ class JobOffer
     public function setDescription(string $description): self
     {
         $this->description = $description;
+
+        return $this;
+    }
+
+    public function getDate(): ?DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(DateTimeInterface $date): self
+    {
+        $this->date = $date;
 
         return $this;
     }
