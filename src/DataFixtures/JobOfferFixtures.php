a1c1167905c179e64b9d4b3d3995a4c596710b36
Arthur GOUAUX <arthur.gouaux@gmail.com>
2022-01-04 21:19:12 +0100

    add description from database

--- src/DataFixtures/JobOfferFixtures.php
+++ src/DataFixtures/JobOfferFixtures.php
@@ -6,28 +6,56 @@ use App\Entity\JobOffer;
 use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+
 class JobOfferFixtures extends Fixture
 {
     public const JOBOFFER = [
         [
             'title' => 'Offre 1',
-            'description' => 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.'
+            'description' => 'At vero eos et accusamus et iusto odio dignissimos ducimus qui 
+            blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias 
+            excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia 
+            deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis 
+            est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque 
+            nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, 
+            omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum 
+            necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. 
+            Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores 
+            alias consequatur aut perferendis doloribus asperiores repellat.'
         ],
 
         [
             'title' => 'Offre 2',
-            'description' => 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.'
+            'description' => 'At vero eos et accusamus et iusto odio dignissimos ducimus qui 
+            blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias 
+            excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia 
+            deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis 
+            est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque 
+            nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, 
+            omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum 
+            necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. 
+            Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores 
+            alias consequatur aut perferendis doloribus asperiores repellat.'
         ],
 
         [
             'title' => 'Offre 3',
-            'description' => 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.'
+            'description' => 'At vero eos et accusamus et iusto odio dignissimos ducimus qui 
+            blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias 
+            excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia 
+            deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis 
+            est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque 
+            nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, 
+            omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum 
+            necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. 
+            Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores 
+            alias consequatur aut perferendis doloribus asperiores repellat.'
         ],
     ];
 
     public function load(ObjectManager $manager): void
     {
-        foreach (self::JOBOFFER as $key => $show) {
+        foreach (self::JOBOFFER as $show) {
             $jobOffer = new JobOffer();
 
             $jobOffer->setTitle($show['title']);
@@ -37,5 +65,4 @@ class JobOfferFixtures extends Fixture
         }
         $manager->flush();
     }
-
 }
