75a2657c526d8f2ffe126fcaf4c3374ab84f3ba4
Arthur GOUAUX <arthur.gouaux@gmail.com>
2022-01-05 16:18:48 +0100

    using faker

--- src/DataFixtures/JobOfferFixtures.php
+++ src/DataFixtures/JobOfferFixtures.php
@@ -6,63 +6,23 @@ use App\Entity\JobOffer;
 use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class JobOfferFixtures extends Fixture
 {
-    public const JOBOFFER = [
-        [
-            'title' => 'Offre 1',
-            'description' => 'At vero eos et accusamus et iusto odio dignissimos ducimus qui 
-            blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias 
-            excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia 
-            deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis 
-            est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque 
-            nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, 
-            omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum 
-            necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. 
-            Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores 
-            alias consequatur aut perferendis doloribus asperiores repellat.'
-        ],
-
-        [
-            'title' => 'Offre 2',
-            'description' => 'At vero eos et accusamus et iusto odio dignissimos ducimus qui 
-            blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias 
-            excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia 
-            deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis 
-            est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque 
-            nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, 
-            omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum 
-            necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. 
-            Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores 
-            alias consequatur aut perferendis doloribus asperiores repellat.'
-        ],
-
-        [
-            'title' => 'Offre 3',
-            'description' => 'At vero eos et accusamus et iusto odio dignissimos ducimus qui 
-            blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias 
-            excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia 
-            deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis 
-            est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque 
-            nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, 
-            omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum 
-            necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. 
-            Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores 
-            alias consequatur aut perferendis doloribus asperiores repellat.'
-        ],
-    ];
+    public const JOB_OFFER = 6;
 
     public function load(ObjectManager $manager): void
     {
-        foreach (self::JOBOFFER as $show) {
+        $faker = Factory::create('fr_FR');
+        for ($i = 0; $i < self::JOB_OFFER; $i++) {
             $jobOffer = new JobOffer();
-
-            $jobOffer->setTitle($show['title']);
-            $jobOffer->setDescription($show['description']);
+            $jobOffer->setTitle($faker->jobTitle());
+            $jobOffer->setDescription($faker->text(500));
             $jobOffer->setDate(new DateTime());
             $manager->persist($jobOffer);
         }
+
         $manager->flush();
     }
 }
