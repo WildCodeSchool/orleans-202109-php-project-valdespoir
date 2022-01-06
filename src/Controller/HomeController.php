7c8ef6aaa1a567a8cbe75aad5b531aa5d7874d0e
cedric-pardon <cedricp02@gmail.com>
2022-01-06 17:57:02 +0100

    CAROUSEL_ACTIVITY

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,15 +6,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\SiteRepository;
+use App\Repository\ActualityRepository;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index(SiteRepository $siteRepository): Response
+    public function index(SiteRepository $siteRepository, ActualityRepository $actualityRepository): Response
     {
         $sites = $siteRepository->findAll();
-        return $this->render('home/index.html.twig', ['sites' => $sites]);
+        $actualitys = $actualityRepository->findAll();
+        return $this->render('home/index.html.twig', ['sites' => $sites, 'actualities' => $actualitys]);
     }
 }
