cce987e1b65caa2024f1bfef8c3c38af0e984666
Martin Lerude <martin.lerude@gmail.com>
2022-01-06 10:59:13 +0100

    add new link

--- src/Controller/PartnerController.php
+++ src/Controller/PartnerController.php
@@ -6,10 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/partner", name="partner")
+ */
 class PartnerController extends AbstractController
 {
     /**
-     * @Route("/partner", name="partner")
+     * @Route("/", name="index")
      */
     public function index(): Response
     {
