884c20af460fcbfab585677eab855d98b7e0acde
Martin Lerude <martin.lerude@gmail.com>
2022-01-10 11:52:42 +0100

    passage de dur à BDD pour les Logos

--- src/Controller/PartenaireController.php
+++ src/Controller/PartenaireController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
+use App\Repository\PartenaireRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+     * @Route("/partner", name="partner")
+     */
 class PartenaireController extends AbstractController
 {
     /**
-     * @Route("/partner", name="partner")
+     * @Route("/", name="_index")
      */
-    public function index(): Response
+    public function index(PartenaireRepository $partenaireRepository): Response
     {
-        return $this->render('partner/index.html.twig');
+        $partenaires = $partenaireRepository->findAll();
+        return $this->render('partner/index.html.twig', ['partenaires' => $partenaires]);
     }
 }
