0a813a023fb1fc5572cda277022bb2b5d61cdeaf
Martin Lerude <martin.lerude@gmail.com>
2022-01-12 22:04:27 +0100

    create partner crud

---
+++ src/Controller/AdminPartnerController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Partner;
+use App\Form\PartnerType;
+use App\Repository\PartnerRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin/partner")
+ */
+class AdminPartnerController extends AbstractController
+{
+    /**
+     * @Route("/", name="admin_partner_index", methods={"GET"})
+     */
+    public function index(PartnerRepository $partnerRepository): Response
+    {
+        return $this->render('admin_partner/index.html.twig', [
+            'partners' => $partnerRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="admin_partner_new", methods={"GET", "POST"})
+     */
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $partner = new Partner();
+        $form = $this->createForm(PartnerType::class, $partner);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($partner);
+            $entityManager->flush();
+            $this->addFlash('succes', 'Le partenaire a bien été ajouté');
+
+            return $this->redirectToRoute('admin_partner_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('admin_partner/new.html.twig', [
+            'partner' => $partner,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="admin_partner_show", methods={"GET"})
+     */
+    public function show(Partner $partner): Response
+    {
+        return $this->render('admin_partner/show.html.twig', [
+            'partner' => $partner,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="admin_partner_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, Partner $partner, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(PartnerType::class, $partner);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('admin_partner_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('admin_partner/edit.html.twig', [
+            'partner' => $partner,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/supprimer", name="delete", methods={"POST"})
+     */
+    public function delete(Request $request, Partner $partner, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $partner->getId(), (string) $request->request->get('_token'))) {
+            $entityManager->remove($partner);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('admin_partner_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
