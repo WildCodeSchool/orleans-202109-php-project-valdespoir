1b0c0df2f4b9c95e9487c97a9aad952c39002f84
Martin Lerude <martin.lerude@gmail.com>
2022-01-12 14:40:27 +0100

    add fixtures 2

--- src/DataFixtures/PartnerFixtures.php
+++ src/DataFixtures/PartnerFixtures.php
@@ -9,18 +9,26 @@ use Doctrine\Persistence\ObjectManager;
 class PartnerFixtures extends Fixture
 {
     public const PARTNERS = [
-        ['name' => 'Région Centre Val de Loire',
-        'https://www.tiralarc-centrevaldeloire.fr/media/uploaded/s
+        [
+            'name' => 'Région Centre Val de Loire',
+            'picture' => 'https://www.tiralarc-centrevaldeloire.fr/media/uploaded/s
         ites/12275/partenaire/58efe73576307_LogoRegionCentreValdeloire.jpg',
-        'https://www.centre-valdeloire.fr/'],
-        ['name' => 'Agglomération Orléans', 'https://upload.wikimedia.org/wikipedia/fr/d/dd/Agglo_orl%C3%A9ans.jpg',
-        'https://www.orleans-metropole.fr/'],
-        ['name' => 'Conseil Départementale', 'https://upload.wikimedia.org/wikipedia/fr/e/e0/Loiret_logo.jpg',
-        'https://www.loiret.fr/'],
+            'link' => 'https://www.centre-valdeloire.fr/'
+        ],
+        [
+            'name' => 'Agglomération Orléans',
+            'picture' => 'https://upload.wikimedia.org/wikipedia/fr/d/dd/Agglo_orl%C3%A9ans.jpg',
+            'link' => 'https://www.orleans-metropole.fr/'
+        ],
+        [
+            'name' => 'Conseil Départementale',
+            'picture' => 'https://upload.wikimedia.org/wikipedia/fr/e/e0/Loiret_logo.jpg',
+            'link' => 'https://www.loiret.fr/'
+        ],
     ];
-    /*public function load(ObjectManager $manager): void
+    public function load(ObjectManager $manager): void
     {
-        foreach (self::PARTNERS as $key => $partners) {
+        foreach (self::PARTNERS as $partners) {
             $partner = new Partner();
             $partner->setName($partners['name']);
             $partner->setPicture($partners['picture']);
@@ -28,5 +36,5 @@ class PartnerFixtures extends Fixture
             $manager->persist($partner);
         }
         $manager->flush();
-    }*/
+    }
 }
