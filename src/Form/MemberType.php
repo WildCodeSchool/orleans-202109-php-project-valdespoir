11efa02b80a93701dd14a8389a95018a1d249072
loick le sausse <rasmo@gmail.com>
2022-01-31 10:26:29 +0100

    fix english name to french

--- src/Form/MemberType.php
+++ src/Form/MemberType.php
@@ -6,16 +6,17 @@ use App\Entity\Member;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class MemberType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('role')
-            ->add('description')
-        ;
+            ->add('name', TextType::class, ['label' => 'Nom',])
+            ->add('role', TextType::class, ['label' => 'Rôle',])
+            ->add('description');
     }
 
     public function configureOptions(OptionsResolver $resolver): void
